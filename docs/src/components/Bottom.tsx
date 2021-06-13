0549f3a5cb8c9253bc7133f3fa2828aa318d5f0e
maayanalbert <maayan@lumilabs.com>
2021-06-12 22:14:49 -0400

    changed site folder

--- docs/src/components/Bottom.tsx
+++ docs/src/components/Bottom.tsx
@@ -4,7 +4,9 @@ export default function Bottom() {
   return (
     <div className="pt-4 pb-16 flex justify-end">
       <div className="text-lg font-light">
-        {"Site made by Maayan with help from Marisa <3"}
+        {
+          "Site made by Maayan one day when she was bored and too hungover to leave the house <3"
+        }
       </div>
     </div>
   );
