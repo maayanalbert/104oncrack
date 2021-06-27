489afb040809b279707c53ce03164e7b2bda114f
maayanalbert <maayan@lumilabs.com>
2021-06-27 00:52:45 -0400

    just getting started

---
+++ sketches/in-progress/flocking/sketch.js
@@ -0,0 +1,27 @@
+let flock;
+
+function setup() {
+  createCanvas(640, 360);
+  createP("Drag the mouse to generate new boids.");
+
+  flock = new Flock();
+  // Add an initial set of boids into the system
+  for (let i = 0; i < 100; i++) {
+    let b = new Boid(width / 2, height / 2);
+    flock.addBoid(b);
+  }
+}
+
+function draw() {
+  background(51);
+  flock.run();
+}
+
+// Add a new boid into the System
+function mouseDragged() {
+  flock.addBoid(new Boid(mouseX, mouseY));
+}
+
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
